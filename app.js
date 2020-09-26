9db29c777ee74f3c6d501f3eb13f4bbcb9a0a756
varunbhalla19 <varunbhalla19@gmail.com>
2020-09-26 10:03:23 +0530

    fixed Nav Transparency and Mail issues

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+particlesJS.load("particles-js", "particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+const navContactLinks = document.getElementById("nav-contact-links");
+
+window.addEventListener("scroll", (ev) => {
+  if (window.scrollY > 5) {
+    if (navContactLinks.classList.contains("transparent")) {
+      navContactLinks.classList.remove("transparent");
+    } else return;
+  } else {
+    if (!navContactLinks.classList.contains("transparent")) {
+      navContactLinks.classList.add("transparent");
+    } else return;
+  }
+});
